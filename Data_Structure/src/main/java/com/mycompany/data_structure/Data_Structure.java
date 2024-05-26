4484ccdd59a553a9fbe12118f365a1990c5e829a
Rushi <rushikesh.21910052@viit.ac.in>
2024-05-26 18:11:15 +0530

    Add a print

--- Data_Structure/src/main/java/com/mycompany/data_structure/Data_Structure.java
+++ Data_Structure/src/main/java/com/mycompany/data_structure/Data_Structure.java
@@ -1,10 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.data_structure;
-
 /**
  *
  * @author Rushikesh Bhondave
@@ -13,8 +7,7 @@ public class Data_Structure {
 
     public static void main(String[] args)
     {
-        System.out.println("Hi Start the data struture");
-                
-         System.out.println("Hi Start the data struture");
+        
+        
     }
 }
