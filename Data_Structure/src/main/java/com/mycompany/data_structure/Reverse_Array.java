280d3908e360dc4b33b6dd110d370747676f083f
Rushi <rushikesh.21910052@viit.ac.in>
2024-06-02 12:06:18 +0530

    Add swtich  and 2 logic add but charAt logic reaming

--- Data_Structure/src/main/java/com/mycompany/data_structure/Reverse_Array.java
+++ Data_Structure/src/main/java/com/mycompany/data_structure/Reverse_Array.java
@@ -1,47 +1,68 @@
 package com.mycompany.data_structure;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Reverse_Array {
 
     public static void main(String[] args) {
-
         Scanner obj = new Scanner(System.in);
-
         System.out.println("Enter the  size:");
         int n = obj.nextInt();
-        String[] arr = new String[n];
-      
+        int[] arr = new int[n], revrse;
+
         System.out.println("Enter the arrys element  " + n + " :");
         for (int i = 0; i < n; i++) {
-            arr[i] = obj.next();
+            arr[i] = obj.nextInt();
 
         }
-          char rev = 0;
-         for(int i=0;i<n;i++)
-        {
-             rev = arr[i].charAt(i);
-        }
-         
-         System.out.println("  Revese arrys element   :"+rev);
-        
-//        int temp;
-//        
-//        for(int i=0;i<n;i++)
-//        {
-//            temp=arr[i];
-//            arr[i]=arr[n-i-1];
-//            arr[n-i-1]=temp;
-//            
-//            if(i!=n-1)
-//            {
-//                    System.out.println("  Revese arrys element   :"+arr[i]);
-//            }
-//            
-//         
-//        }
 
-        
+        System.out.println("Enter the method 1.Swap with temp 2.CharAt 3.Revesr arrat print ");
+        int ch = obj.nextInt();
+
+        switch (ch) {
+            case 1: {
+                int temp;
+
+                for (int i = 0; i < n / 2; i++) {
+                    temp = arr[i];
+                    arr[i] = arr[n - i - 1];
+                    arr[n - i - 1] = temp;
+
+                }
+
+                System.out.println("  Revese arrys element   :");
+                for (int i = 0; i < n; i++) {
+
+                    System.out.println(arr[i]);
+                }
+            }
+            break;
+
+            case 2: {
+                String rev;
+                for (int i = 0; i < n ; i++) {
+
+               
+
+                }
+            }
+            break;
+
+            case 3: {
+                System.out.println("Original Array printed in reverse order:");
+                for (int i = arr.length - 1; i >= 0; i--) {
+                    System.out.print(arr[i] + "  ");
+                }
+            }
+
+            break;
+
+            default:
+                System.out.println("invalid No");
+
+        }
 
     }
 
