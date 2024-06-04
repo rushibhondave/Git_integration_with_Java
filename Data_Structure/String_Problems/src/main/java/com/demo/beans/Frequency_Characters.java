f299c0a5441de6aa51c26277793b2facc7bc4b66
Rushi <rushikesh.21910052@viit.ac.in>
2024-06-04 23:07:11 +0530

    Frequency_Characters  count

---
+++ Data_Structure/String_Problems/src/main/java/com/demo/beans/Frequency_Characters.java
@@ -0,0 +1,18 @@
+package com.demo.beans;
+
+import java.util.Scanner;
+
+public class Frequency_Characters {
+
+    public static void CountChar() 
+    {
+        Scanner obj = new Scanner(System.in);
+        
+        System.out.println("Enter the String");
+        String str=obj.nextLine();
+        
+        System.out.println(str);
+
+    }
+
+}
---
+++ Data_Structure/String_Problems/src/main/java/com/mycompany/TestMain/String_Problems.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.TestMain;
+
+import com.demo.beans.Frequency_Characters;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Rushikesh Bhondave
+ */
+public class String_Problems {
+
+    public static void main(String[] args)
+    {
+           Scanner obj = new Scanner(System.in);
+         System.out.println("Enter the choice 1.Count Character 2. Remove Characters");
+         int Ch=obj.nextInt();
+         
+         switch (Ch) {
+            case 1 ->
+            {
+                Frequency_Characters.CountChar();
+                break;
+            }
+            
+            default -> throw new AssertionError();
+        }
+        
+    }
+}
