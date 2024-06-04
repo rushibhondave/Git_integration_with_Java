8c99a2f5cca2e7abe358fda6930beaf4b1e8a796
Rushi <rushikesh.21910052@viit.ac.in>
2024-06-04 23:58:40 +0530

    Frequency_Characters count

--- Data_Structure/String_Problems/src/main/java/com/demo/beans/Frequency_Characters.java
+++ Data_Structure/String_Problems/src/main/java/com/demo/beans/Frequency_Characters.java
@@ -8,9 +8,24 @@ public class Frequency_Characters {
     {
         Scanner obj = new Scanner(System.in);
         
+        int [] count=new int [256];
+ 
         System.out.println("Enter the String");
         String str=obj.nextLine();
         
+        for(int i=0;i<str.length();i++)
+        {
+            count[ str.charAt(i)]++;
+        }
+        
+        for(int i=0;i<count.length;i++)
+        {
+            if(count[i]>0)
+            {
+                 System.out.println((char)i + ": " + count[i]);
+            }
+                
+        }
         System.out.println(str);
 
     }
